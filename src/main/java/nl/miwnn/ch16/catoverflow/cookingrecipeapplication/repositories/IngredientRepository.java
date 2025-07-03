e1e3d2290dd4a9e686f5a39e372e8c1df2223c9e
RobynBlignaut <R_Blignaut@hotmail.com>
2025-07-03 16:47:04 +0200

    Added findByIngredientName to IngredientRepository

--- src/main/java/nl/miwnn/ch16/catoverflow/cookingrecipeapplication/repositories/IngredientRepository.java
+++ src/main/java/nl/miwnn/ch16/catoverflow/cookingrecipeapplication/repositories/IngredientRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Optional<Ingredient> findByIngredientName(String name);
+
     Optional<Ingredient> findByIngredientId(Long ingredientId);
 }
