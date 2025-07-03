dc689e6f942d11bafdd36a92bf3de6fba322d7f0
RobynBlignaut <R_Blignaut@hotmail.com>
2025-07-03 16:49:03 +0200

    Added overview, new, edit, save and delete to the ingredientController.

--- src/main/java/nl/miwnn/ch16/catoverflow/cookingrecipeapplication/controller/IngredientController.java
+++ src/main/java/nl/miwnn/ch16/catoverflow/cookingrecipeapplication/controller/IngredientController.java
@@ -1,7 +1,14 @@
 package nl.miwnn.ch16.catoverflow.cookingrecipeapplication.controller;
 
+import nl.miwnn.ch16.catoverflow.cookingrecipeapplication.model.Ingredient;
+import nl.miwnn.ch16.catoverflow.cookingrecipeapplication.repositories.IngredientRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * @author Robyn Blignaut, Bas Folkers
@@ -12,4 +19,77 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/ingredient")
 public class IngredientController {
 
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    private String setupIngredientOverview(Model datamodel, Ingredient formIngredient, boolean formModalHidden) {
+        datamodel.addAttribute("allIngredients", ingredientRepository.findAll());
+        datamodel.addAttribute("formIngredient", formIngredient);
+        datamodel.addAttribute("formModalHidden", formModalHidden);
+
+        return "ingredientOverview";
+    }
+
+//    private String setupIngredientDetail(Model datamodel, Ingredient ingredientToShow, Ingredient formIngredient, boolean formModalHidden) {
+//        datamodel.addAttribute("ingredient", ingredientToShow);
+//        datamodel.addAttribute("formIngredient", formIngredient);
+//        datamodel.addAttribute("formModalHidden", formModalHidden);
+//
+//        return "ingredientDetails";
+//    }
+
+    @GetMapping("/overview")
+    public String showIngredientOverview(Model datamodel) {
+        datamodel.addAttribute("allIngredients", ingredientRepository.findAll());
+        return setupIngredientOverview(datamodel, new Ingredient(), true);
+    }
+
+    @GetMapping("/new")
+    private String newIngredient(Model datamodel) {
+        datamodel.addAttribute("ingredientForm", new Ingredient());
+        return "ingredientForm";
+    }
+
+    @GetMapping("/edit/{ingredientId}")
+    private String editIngredient(@PathVariable("ingredientId") Long ingredientId, Model datamodel) {
+        Optional<Ingredient> ingredientOptional = ingredientRepository.findById(ingredientId);
+        if (ingredientOptional.isPresent()) {
+            datamodel.addAttribute("ingredientForm", ingredientOptional.get());
+        }
+        return "ingredientForm";
+    }
+
+    @PostMapping("/save")
+    private String saveOrUpdateIngredient(@ModelAttribute("ingredientForm") Ingredient ingredientToBeSaved,
+                                          BindingResult result,
+                                          Model datamodel) {
+
+        if (result.hasErrors() && ingredientToBeSaved.getIngredientId() != null) {
+            Ingredient originalIngredientDetails = ingredientRepository
+                                                    .findById(ingredientToBeSaved.getIngredientId())
+                                                    .orElse(new Ingredient());
+
+//            return setupIngredientDetail(datamodel, originalIngredientDetails, ingredientToBeSaved, false);
+        } else if (result.hasErrors()) {
+            return setupIngredientOverview(datamodel, ingredientToBeSaved, false);
+        }
+        ingredientRepository.save(ingredientToBeSaved);
+        return "redirect:/ingredient/overview";
+    }
+
+    private void checkIngredientNameIsUnique(Ingredient ingredientToBeSaved, BindingResult result) {
+        Optional<Ingredient> sameName = ingredientRepository.findByIngredientName(ingredientToBeSaved.getIngredientName());
+        if (sameName.isPresent() && !sameName.get().getIngredientId().equals(ingredientToBeSaved.getIngredientId())) {
+            result.addError(new FieldError("ingredient", "name", "this name is already in use"));
+        }
+    }
+
+    @GetMapping("/delete/{ingredientName}")
+    private String deleteIngredient(@PathVariable("ingredientName") String name) {
+        ingredientRepository.findByIngredientName(name).ifPresent(ingredientRepository::delete);
+        return "redirect:/ingredient/overview";
+    }
 }
